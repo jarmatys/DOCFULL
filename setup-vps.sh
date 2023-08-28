6bb0f454841df6cdf0992d3aeb6bfbcc7f001b02
Jarek Armatys <jarekarmatys@gmail.com>
2023-08-28 18:44:12 +0200

    Added webhook app

--- setup-vps.sh
+++ setup-vps.sh
@@ -33,12 +33,13 @@ mkdir -p /var/opeenet/ # TODO: Nazwa aplikacji do zmiennej
 mkdir -p /var/common/vps/ 
 mkdir -p /var/common/app/ 
 
-# 3.1 Add permissions to folders
-
-sudo chmod 777 /var/opeenet/ # TODO: Nazwa aplikacji do zmiennej
-sudo chmod -R 777 /var/common/
+# 3.1 Add docker network
+docker network create vps-network
 
 # 4. Copy docker-composes files to proper folders
+
+cp DOCKER/.env /var/common/app/.env
+cp DOCKER/.env /var/common/vps/.env
 
 cp DOCKER/docker-compose.mssql.yml /var/common/app/docker-compose.mssql.yml
 cp DOCKER/docker-compose.rabbit.yml /var/common/app/docker-compose.rabbit.yml
@@ -51,6 +52,22 @@ cp DOCKER/docker-compose.portainer.yml /var/common/vps/docker-compose.portainer.
 
 ssh-keygen -m PEM -t rsa -b 4096
 cat id_rsa.pub >> ~/.ssh/authorized_keys
+
+# ?. Add permissions to folders
+
+sudo chmod 777 /var/opeenet/ # TODO: Nazwa aplikacji do zmiennej
+sudo chmod -R 777 /var/common/
+
+# ?. Run all needed containers
+
+cd /var/common/app/
+sudo docker compose -f docker-compose.rabbit.yml up -d
+sudo docker compose -f docker-compose.mssql.yml up -d
+sudo docker compose -f docker-compose.seq.yml up -d
+
+cd /var/common/vps/
+sudo docker compose -f docker-compose.nginx.yml up -d
+sudo docker compose -f docker-compose.portainer.yml up -d
 
 # -----
 
