df1775477e654411db372b42f3ed67037dc1ea31
Jarek Armatys <jarekarmatys@gmail.com>
2023-09-17 15:00:22 +0200

    Added docker compose local setup

---
+++ LOCAL/start-local-setup.ps1
@@ -0,0 +1,1 @@
+docker compose -f .\docker-compose.mssql.yml -f .\docker-compose.rabbit.yml -f .\docker-compose.seq.yml -f .\docker-compose.proxy.yml -f .\docker-compose.typesense.yml -p=local up -d
