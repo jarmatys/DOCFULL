126e579da0144b4841f64cdc5ccde5662f768300
Jarosław Armatys <jarekarmatys@gmail.com>
2024-07-08 15:02:25 +0200

    Updated local setup

--- LOCAL/start-local-setup.ps1
+++ LOCAL/start-local-setup.ps1
@@ -1,1 +1,37 @@
-docker compose -f .\docker-compose.mssql.yml -f .\docker-compose.rabbit.yml -f .\docker-compose.seq.yml -f .\docker-compose.proxy.yml -f .\docker-compose.typesense.yml -p=local up -d
+param (
+    [switch] $mssql,
+    [switch] $proxy,
+    [switch] $qdrant,
+    [switch] $rabbit,
+    [switch] $seq,
+    [switch] $typesense
+)
+
+if ($mssql)
+{
+    docker compose -f .\docker-compose.mssql.yml up -d
+}
+elseif ($proxy)
+{
+    docker compose -f .\docker-compose.proxy.yml up -d
+}
+elseif ($qdrant)
+{
+    docker compose -f .\docker-compose.qdrant.yml up -d
+}
+elseif ($rabbit)
+{
+    docker compose -f .\docker-compose.rabbit.yml up -d
+}
+elseif ($seq)
+{
+    docker compose -f .\docker-compose.seq.yml up -d
+}
+elseif ($typesense)
+{
+    docker compose -f .\docker-compose.typesense.yml up -d
+}
+else
+{
+    Write-Host "Please provide a profile to start the enviroment: -mssql, -proxy, -qdrant, -rabbit, -seq, -typesense"
+}
